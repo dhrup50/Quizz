a3c19c6f5fbed5be7d9340dce6c3fd708240d3d4
DHRUPCHAND YADAV <dhupchand585858@gmail.com>
2026-02-14 06:55:29 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,121 @@
+
+const data = [
+  {
+    question: "Cat : Kitten :: Dog : ?",
+    option1: "Cub",
+    option2: "Calf",
+    option3: "Puppy",
+    option4: "Foal",
+    ans: 3,
+  },
+  {
+    question: "Apple, Banana, Mango, Potato ?",
+    option1: "Apple",
+    option2: "Banana",
+    option3: "Mango",
+    option4: "Potato",
+    ans: 4,
+  },
+  {
+     
+    question: "2, 4, 8, 16, ?",
+    option1: "18",
+    option2: "24",
+    option3: "30",
+     option4:"32",
+    ans: 4,
+  },
+
+  {
+    id: 4,
+    question:"If CAT is written as DBU, how is DOG written?",
+    option1: "EPH",
+    option2: "EOG",
+    option3: "FPH",
+    option4: "DNH",
+    ans: 1,
+  },
+
+  {
+    id: 5,
+    question: "Ram is the brother of Sita. Sita is the mother of Aman. How is Ram related to Aman?",
+    option1: "Father",
+    option2: "Uncle",
+    option3: "Brother",
+    option4: "Grandfather",
+    ans: 2,
+  },
+
+  {
+    id: 6,
+    question: "Rahul walks 10 m east and then 5 m north. How far is he from the starting point?",
+    option1: "15 m",
+    option2: "10 m",
+    option3: "12 m",
+    option4: "11.18 m",
+    ans: 4,
+  },
+
+  {
+    id: 7,
+    question: "In a class of 20 students, Ravi is 5th from the top. What is his rank from the bottom?",
+    option1:"14",
+    option2:"15",
+    option3: "16",
+    option4:"17",
+    ans: 3,
+  },
+
+  {
+     
+    question: " Pen, Pencil, Eraser, Book",
+    option1: "Pen",
+    option2: "Pencil",
+    option3: "Eraser",
+    option4: "Book",
+    ans: 4,
+  },
+
+  {
+    
+    question: "A, D, G, J, ?",
+    option1:"K",
+    option2: "L",
+    option3:"M",
+    option4: "N",
+    ans: 3,
+  },
+
+  {
+    
+    question: "Statement: All dogs are animals./Conclusion: All animals are dogs.",
+    option1:"True",
+    option2: "False",
+    option3: "Both true",
+    option4: "Both false",
+    ans: 2,
+  },
+
+  {
+     
+    question: "If 5 + 3 = 28 and 7 + 2 = 63, then 6 + 4 = ?",
+    option1: "40",
+    option2: "50",
+    option3: "60",
+    option4: "70",
+    ans: 3,
+  },
+
+  {
+  
+    question: "What is the angle between the hands of a clock at 3:00?",
+     option1: "60°",
+     option2: "90°",
+     option3: "120°",
+     option4: "180°",
+    ans: 2,
+  },
+];
+
+export default data;
+
